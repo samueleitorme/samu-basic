d9e1df112843eb9339db720fb219b048284e2d4e
samuelreche-smr <98898781+samuelreche-smr@users.noreply.github.com>
2022-02-07 18:36:09 +0100

    Delete samu-basic.sh

--- samu-basic.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo Actualización e instalación de paquetes.
-echo Nmap, net-tools, Filezilla, ftp, figlet, gnome-teaks, remmina
-echo ------------------------------------------------------------------------------
-sudo apt update; sudo apt upgrade
-sudo apt install nmap net-tools gnome-tweak-tool remmina
-sudo apt install filezilla ftp ssh figlet
-echo Listo.
-return
