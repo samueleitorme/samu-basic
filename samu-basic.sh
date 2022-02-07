94a610edcffdc83a449a7a1bba334392db01bcf2
samuelreche-smr <98898781+samuelreche-smr@users.noreply.github.com>
2022-02-07 18:55:07 +0100

    Update samu-basic.sh

--- samu-basic.sh
+++ samu-basic.sh
@@ -2,7 +2,7 @@
 echo Actualización e instalación de paquetes.
 echo Nmap, net-tools, Filezilla, ftp, figlet, gnome-teaks, remmina
 echo ------------------------------------------------------------------------------
-sudo apt update; sudo apt upgrade
+sudo apt update && sudo apt upgrade
 sudo apt install nmap net-tools gnome-tweak-tool remmina filezilla ftp openssh-client figlet -y
 echo Todo listo.
 return
