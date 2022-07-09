6af748dc86841b7ec26317d48a1c9ed93b8fbb29
Samuel Reche <98898781+samueleitorme@users.noreply.github.com>
2022-07-09 02:46:15 +0200

    Update samu-vboxg1.sh

--- samu-vboxg1.sh
+++ samu-vboxg1.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo INSTALANDO VIRTUALBOX GUEST ADDITIONS
 sudo apt install build-essential linux-headers-$(uname -r) dkms -y
-echo Ahora tienes que reiniciar, una vez reinicies, ejecuta la parte 2.
-return
+echo Ahora se reiniciará, una vez reiniciado, ejecuta la parte 2.
+sudo reboot -t 10
